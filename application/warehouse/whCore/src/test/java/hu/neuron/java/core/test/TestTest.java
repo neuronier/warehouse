57155ca8db59cf616f3b41310893e8dcc8b45000
Tóth Dávid <skot9212@gmail.com>
2015-08-06 16:38:14 +0200

    riportok módosítva

---
+++ application/warehouse/whCore/src/test/java/hu/neuron/java/core/test/TestTest.java
@@ -0,0 +1,16 @@
+package hu.neuron.java.core.test;
+
+import org.junit.*;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+
+public class TestTest {
+
+	@Test
+	public void test() {
+	
+	}
+
+}
