65bfba96a9eead79d7d17e4f65b349c8738fd9f9
Tóth Dávid <skot9212@gmail.com>
2015-08-08 12:47:10 +0200

    Warehouse entity és dao, még nincs teljesen befejezve...

---
+++ application/warehouse/whCore/src/main/java/hu/neuron/java/warehouse/whCore/entity/Warehouse.java
@@ -0,0 +1,93 @@
+package hu.neuron.java.warehouse.whCore.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Warehouse")
+public class Warehouse extends BaseEntity  {
+
+	
+	private static final long serialVersionUID = 1L;
+	/**
+	 * Raktár neve.
+	 */
+	private String name;
+	/**
+	 * Raktár azonosítója.
+	 */
+	private Long warehouseId;
+	/**
+	 * Raktár irányitószáma.
+	 */
+	private int zipCode;
+	/**
+	 * Raktár települése.
+	 */
+	private String city;
+	/**
+	 * Utca.
+	 */
+	private String address;
+	/**
+	 *  Házszám.
+	 */
+	private int addressNumber;
+	/**
+	 * Raktáros(egy már beregisztrált felhasználó).
+	 */
+	private Long managerId;
+	
+	
+	public Long getManagerId() {
+		return managerId;
+	}
+	public void setManagerId(Long managerId) {
+		this.managerId = managerId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Long getWarehouseId() {
+		return warehouseId;
+	}
+	public void setWarehouseId(Long warehouseId) {
+		this.warehouseId = warehouseId;
+	}
+	public int getZipCode() {
+		return zipCode;
+	}
+	public void setZipCode(int zipCode) {
+		this.zipCode = zipCode;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public int getAddressNumber() {
+		return addressNumber;
+	}
+	public void setAddressNumber(int addressNumber) {
+		this.addressNumber = addressNumber;
+	}
+	@Override
+	public String toString() {
+		return "Warehouse [name=" + name + ", warehouseId=" + warehouseId
+				+ ", zipCode=" + zipCode + ", city=" + city + ", address="
+				+ address + ", addressNumber=" + addressNumber + ", manager="
+				+ managerId + "]";
+	}
+	
+}
---
+++ application/warehouse/whCore/src/test/java/hu/neuron/java/warehouse/whCore/dao/WarehouseDaoTest.java
@@ -0,0 +1,83 @@
+package hu.neuron.java.warehouse.whCore.dao;
+
+import hu.neuron.java.warehouse.whCore.entity.Warehouse;
+
+import org.apache.log4j.Logger;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.transaction.TransactionConfiguration;
+import org.springframework.transaction.annotation.Transactional;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("/spring-test-core.xml")
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+@Transactional
+@TransactionConfiguration(defaultRollback = false)
+public class WarehouseDaoTest {
+
+	private static final Logger logger = Logger
+			.getLogger(WarehouseDaoTest.class);
+
+	private static Warehouse warehouse;
+
+	@Autowired
+	WarehouseDao warehouseDao;
+
+	@Test
+	public void test1Save() {
+
+		try {
+			warehouse = new Warehouse();
+
+			warehouse.setAddress("sd");
+			warehouse.setAddressNumber(12);
+			warehouse.setCity("vbcvbcv");
+			warehouse.setManagerId(2l);
+			warehouse.setName("naeyxc");
+			warehouse.setWarehouseId(1l);
+			warehouse.setZipCode(1234);
+
+			warehouse = warehouseDao.save(warehouse);
+		} catch (Exception e) {
+			logger.error(e.getMessage(), e);
+			throw new RuntimeException(e);
+		}
+
+	}
+
+	@Test
+	public void test2Update() {
+		try {
+			warehouse = warehouseDao.findOne(warehouse.getId());
+
+			warehouse.setAddress("Böszörményi út");
+			warehouse.setAddressNumber(35);
+			warehouse.setCity("Debrecen");
+			warehouse.setManagerId(2l);
+			warehouse.setName("Központ");
+			warehouse.setWarehouseId(1l);
+			warehouse.setZipCode(4321);
+
+			warehouse = warehouseDao.save(warehouse);
+		} catch (Exception e) {
+			logger.error(e.getMessage(), e);
+			throw new RuntimeException(e);
+		}
+	}
+
+	@Test
+	public void test3Delete() {
+		try {
+			warehouseDao.delete(warehouse.getId());
+		} catch (Exception e) {
+			logger.error(e.getMessage(), e);
+			throw new RuntimeException(e);
+		}
+	}
+
+}
