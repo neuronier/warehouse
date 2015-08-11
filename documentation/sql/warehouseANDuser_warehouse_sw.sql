58a1f29754f5f02f050e274266c511993e87afa5
Tóth Dávid <skot9212@gmail.com>
2015-08-11 07:41:08 +0200

    sql update

--- documentation/sql/warehouseANDuser_warehouse_sw.sql
+++ documentation/sql/warehouseANDuser_warehouse_sw.sql
@@ -1,11 +1,11 @@
 CREATE TABLE IF NOT EXISTS `Warehouse` (
   `id` BIGINT(20) NOT NULL COMMENT '',
-  `warehouse_id` BIGINT(20) NOT NULL UNIQUE COMMENT '',
+  `warehouseId` BIGINT(20) NOT NULL UNIQUE COMMENT '',
   `name` VARCHAR(255) NULL COMMENT '',
-  `zip_ode` INT(6) NULL COMMENT '',
+  `zipCode` INT(6) NULL COMMENT '',
   `city` VARCHAR(255) NULL COMMENT '',
   `address` VARCHAR(255) NULL COMMENT '',
-  `address_number` int(6) NULL COMMENT '',
+  `addressNumber` int(6) NULL COMMENT '',
   `users` BIGINT(20) NULL COMMENT '',
   PRIMARY KEY (`id`)  COMMENT '')
 ENGINE = InnoDB
