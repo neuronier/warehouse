65d60f8795afd312e1f584d14131937d00cdd39f
Veress Balázs <veress.balazs1993@gmail.com>
2015-08-13 17:56:36 +0200

    ware.sql feltöltése
    Signed-off-by: Veress Balázs <veress.balazs1993@gmail.com>

---
+++ documentation/sql/ware.sql
@@ -0,0 +1,8 @@
+INSERT INTO `ware`(`id`, `description`, `itemNumber`, `wareName`)
+VALUES (1,"itemdesc1",1,"item1");
+INSERT INTO `ware`(`id`, `description`, `itemNumber`, `wareName`) 
+VALUES (2,"itemdesc2",2,"item2");
+INSERT INTO `ware`(`id`, `description`, `itemNumber`, `wareName`) 
+VALUES (3,"itemdesc3",3,"item3");
+INSERT INTO `ware`(`id`, `description`, `itemNumber`, `wareName`) 
+VALUES (4,"itemdesc4",4,"item4");
