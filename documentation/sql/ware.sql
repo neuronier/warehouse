ac3ec9d7bdf7457afadf1c30276a882fd75d4efc
Veress Balázs <veress.balazs1993@gmail.com>
2015-08-13 17:59:45 +0200

    ware.sql-kisjavítás
    Signed-off-by: Veress Balázs <veress.balazs1993@gmail.com>

--- documentation/sql/ware.sql
+++ documentation/sql/ware.sql
@@ -1,8 +1,8 @@
-INSERT INTO `ware`(`id`, `description`, `itemNumber`, `wareName`)
-VALUES (1,"itemdesc1",1,"item1");
-INSERT INTO `ware`(`id`, `description`, `itemNumber`, `wareName`) 
-VALUES (2,"itemdesc2",2,"item2");
-INSERT INTO `ware`(`id`, `description`, `itemNumber`, `wareName`) 
-VALUES (3,"itemdesc3",3,"item3");
-INSERT INTO `ware`(`id`, `description`, `itemNumber`, `wareName`) 
-VALUES (4,"itemdesc4",4,"item4");
+INSERT INTO `ware`(`description`, `itemNumber`, `wareName`)
+VALUES ("itemdesc1",1,"item1");
+INSERT INTO `ware`(`description`, `itemNumber`, `wareName`) 
+VALUES ("itemdesc2",2,"item2");
+INSERT INTO `ware`(`description`, `itemNumber`, `wareName`) 
+VALUES ("itemdesc3",3,"item3");
+INSERT INTO `ware`(`description`, `itemNumber`, `wareName`) 
+VALUES ("itemdesc4",4,"item4");
