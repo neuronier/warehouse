2d869cbcd2c83778ed23c25e96cbd2c05acdafd4
Tóth Dávid <skot9212@gmail.com>
2015-08-15 15:09:08 +0200

    sql update

--- documentation/sql/warehouseANDuser_warehouse_sw.sql
+++ documentation/sql/warehouseANDuser_warehouse_sw.sql
@@ -1,27 +1,6 @@
-CREATE TABLE IF NOT EXISTS `Warehouse` (
-  `id` BIGINT(20) NOT NULL COMMENT '',
-  `warehouseId` BIGINT(20) NOT NULL UNIQUE COMMENT '',
-  `name` VARCHAR(255) NULL COMMENT '',
-  `zipCode` INT(6) NULL COMMENT '',
-  `city` VARCHAR(255) NULL COMMENT '',
-  `address` VARCHAR(255) NULL COMMENT '',
-  `addressNumber` int(6) NULL COMMENT '',
-  `users` BIGINT(20) NULL COMMENT '',
-  PRIMARY KEY (`id`)  COMMENT '')
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = latin1;
-
-
-CREATE TABLE IF NOT EXISTS `user_warehouse_sw` (
-  `users_id` BIGINT(20) NOT NULL COMMENT '',
-  `warehouse_id` BIGINT(20) NOT NULL COMMENT '',
-  INDEX `userI` (`warehouse_id` ASC)  COMMENT '',
-  INDEX `warehouseI` (`users_id` ASC)  COMMENT '',
-  CONSTRAINT `warehouseI`
-    FOREIGN KEY (`users_id`)
-    REFERENCES `user` (`id`),
-  CONSTRAINT `userI`
-    FOREIGN KEY (`warehouse_id`)
-    REFERENCES `warehouse` (`id`))
+CREATE TABLE IF NOT EXISTS `warehouse_user` (
+  `warehouseId` BIGINT(20) NOT NULL,
+  `userId` BIGINT(20) NOT NULL ,
+  PRIMARY KEY (warehouseId,userId))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = latin1;
