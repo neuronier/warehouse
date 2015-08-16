82d22d1fd6089b2f8f9f97faa4c4d00f092dd1e4
Abai Richárd <richarddinozzo@gmail.com>
2015-08-16 12:29:06 +0200

    warehouse és ware berakva a transport menükbe (i hope)...
    Modified   application/warehouse/whWeb/src/main/java/hu/neuron/java/warehouse/whWeb/controller/TransportController.java
    Modified   application/warehouse/whWeb/src/main/webapp/transport/transport.xhtml

--- application/warehouse/whWeb/src/main/java/hu/neuron/java/warehouse/whWeb/controller/TransportController.java
+++ application/warehouse/whWeb/src/main/java/hu/neuron/java/warehouse/whWeb/controller/TransportController.java
@@ -1,10 +1,7 @@
 package hu.neuron.java.warehouse.whWeb.controller;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.Collection;
 
-import javax.annotation.PostConstruct;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
@@ -20,10 +17,6 @@ public class TransportController implements Serializable {
 	 */
 	private static final long serialVersionUID = 1L;
 	
-	private Collection<String> warehouses;
-	
-	private Collection<String> wares;
-	
 	private int db;
 
 	public int getDb() {
@@ -33,42 +26,12 @@ public class TransportController implements Serializable {
 	public void setDb(int db) {
 		this.db = db;
 	}
-
-	public Collection<String> getWarehouses() {
-		return warehouses;
-	}
-
-	public void setWarehouses(Collection<String> warehouses) {
-		this.warehouses = warehouses;
-	}
-	
-	public Collection<String> getWares() {
-		return wares;
-	}
-	
-	public void setWares(Collection<String> wares) {
-		this.wares = wares;
-	}
-	
-	@PostConstruct
-    public void init() {
-		
-		warehouses = new ArrayList<String>();
-		warehouses.add("warehouse1");
-		warehouses.add("warehouse2");
-		warehouses.add("warehouse3");
-		
-		wares = new ArrayList<String>();
-		wares.add("ware1");
-		wares.add("ware2");
-		wares.add("ware3");
-	}
 	
 	public void buttonAction(ActionEvent actionEvent) {
         addMessage("A szállítás hamarosan megkezdődik");
     }
      
-    public void addMessage(String summary) {
+    public void addMessage(String summary) {        
         FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, summary,  null);
         FacesContext.getCurrentInstance().addMessage(null, message);
     }
