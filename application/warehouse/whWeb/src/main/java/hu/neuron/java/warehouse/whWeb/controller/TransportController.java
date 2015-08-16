3ab05ea8153c4b658874c9fc6e0b28c27c0cdfd7
Abai Richárd <richarddinozzo@gmail.com>
2015-08-16 15:29:20 +0200

    ápdét
    Modified   application/warehouse/whWeb/src/main/java/hu/neuron/java/warehouse/whWeb/controller/TransportController.java
    Modified   application/warehouse/whWeb/src/main/webapp/transport/transport.xhtml

--- application/warehouse/whWeb/src/main/java/hu/neuron/java/warehouse/whWeb/controller/TransportController.java
+++ application/warehouse/whWeb/src/main/java/hu/neuron/java/warehouse/whWeb/controller/TransportController.java
@@ -1,7 +1,17 @@
 package hu.neuron.java.warehouse.whWeb.controller;
 
-import java.io.Serializable;
+import hu.neuron.java.warehouse.whBusiness.service.WareServiceLocal;
+import hu.neuron.java.warehouse.whBusiness.service.WarehouseServiceLocal;
+import hu.neuron.java.warehouse.whBusiness.vo.WareVo;
+import hu.neuron.java.warehouse.whBusiness.vo.WarehouseVO;
 
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
@@ -16,8 +26,58 @@ public class TransportController implements Serializable {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	
+
 	private int db;
+
+	//warehouseok
+	private Collection<WarehouseVO> warehouses;
+	
+	private Collection<String> whNames;
+
+	@EJB(name = "WarehouseService")
+	WarehouseServiceLocal warehouseService;
+	
+	//itemek
+	private List<WareVo> wares;
+	
+	private Collection<String> wareNames;
+	
+	@EJB(beanName = "WareService")
+	WareServiceLocal wareService;
+
+	@PostConstruct
+	void init() {
+		
+		warehouses = new ArrayList<WarehouseVO>();
+		warehouses = warehouseService.findAll();
+		for (WarehouseVO warehouseVO : warehouses) {
+			whNames.add(warehouseVO.getName());
+		}
+		
+		wares = new ArrayList<WareVo>();
+		wares = wareService.getWares();
+		for (WareVo wareVo : wares) {
+			wareNames.add(wareVo.getWareName());
+		}
+	}
+
+	public void buttonAction(ActionEvent actionEvent) {
+		addMessage("A szállítás hamarosan megkezdődik");
+	}
+
+	public void addMessage(String summary) {
+		FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO,
+				summary, null);
+		FacesContext.getCurrentInstance().addMessage(null, message);
+	}
+
+	public Collection<WarehouseVO> getWarehouses() {
+		return warehouses;
+	}
+
+	public void setWarehouses(Collection<WarehouseVO> warehouses) {
+		this.warehouses = warehouses;
+	}
 
 	public int getDb() {
 		return db;
@@ -26,14 +86,29 @@ public class TransportController implements Serializable {
 	public void setDb(int db) {
 		this.db = db;
 	}
-	
-	public void buttonAction(ActionEvent actionEvent) {
-        addMessage("A szállítás hamarosan megkezdődik");
-    }
-     
-    public void addMessage(String summary) {        
-        FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, summary,  null);
-        FacesContext.getCurrentInstance().addMessage(null, message);
-    }
+
+	public Collection<String> getWhNames() {
+		return whNames;
+	}
+
+	public void setWhNames(Collection<String> whNames) {
+		this.whNames = whNames;
+	}
+
+	public List<WareVo> getWares() {
+		return wares;
+	}
+
+	public void setWares(List<WareVo> wares) {
+		this.wares = wares;
+	}
+
+	public Collection<String> getWareNames() {
+		return wareNames;
+	}
+
+	public void setWareNames(Collection<String> wareNames) {
+		this.wareNames = wareNames;
+	}
 
 }
