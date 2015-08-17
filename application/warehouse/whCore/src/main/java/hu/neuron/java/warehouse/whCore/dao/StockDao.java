ac8229f537c240207d218663f9170e623d77a018
Tóth Dávid <skot9212@gmail.com>
2015-08-17 15:14:34 +0200

    ctrl+v hiba...

--- application/warehouse/whCore/src/main/java/hu/neuron/java/warehouse/whCore/dao/StockDao.java
+++ application/warehouse/whCore/src/main/java/hu/neuron/java/warehouse/whCore/dao/StockDao.java
@@ -12,7 +12,7 @@ public interface StockDao {
 	
 	
 	@Modifying
-	@Query(value = "insert into warehouse_user (warehouseId, userId, piece) VALUES (?1, ?2, ?3)", nativeQuery = true)
+	@Query(value = "insert into stock (warehouseId, wareId, piece) VALUES (?1, ?2, ?3)", nativeQuery = true)
 	void addWareToWarehouse(String warehouseId, int wareId, int piece) throws Exception;
 
 	
