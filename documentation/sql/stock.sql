f83e3eefda6eccbfc36b6450666658157f5bc12d
Tóth Dávid <skot9212@gmail.com>
2015-08-18 10:09:11 +0200

    sql update

--- documentation/sql/stock.sql
+++ documentation/sql/stock.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS `stock` (
-  `id` BIGINT(20) NOT NULL unique ,
+  `id` BIGINT(20) NOT NULL unique auto_increment ,
   `warehouseId` VARCHAR (20) NOT NULL,
   `wareId` BIGINT(20) NOT NULL,
   `piece` INT(6),
