8ebd6165a11ebb24dc4366e26809b42e5a1b2f7a
Abai Richárd <richarddinozzo@gmail.com>
2015-08-20 11:46:01 +0200

    elsődleges kulcs csere

--- documentation/sql/transport_details.sql
+++ documentation/sql/transport_details.sql
@@ -2,6 +2,6 @@ CREATE TABLE IF NOT EXISTS `transport_details` (
   `transport_id` BIGINT(20) NOT NULL unique ,
   `wareId` varchar(255) NOT NULL,
   `piece` BIGINT(10),
-  PRIMARY KEY (from_warehouseId, to_warehouseId, wareId))
+  PRIMARY KEY (transport_id))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = latin1;
