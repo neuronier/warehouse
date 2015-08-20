b6fe0dd0179dbab77f6c8f6d67cdd0eda11d42e1
Abai Richárd <richarddinozzo@gmail.com>
2015-08-20 11:31:50 +0200

    sql update

--- documentation/sql/transport_details.sql
+++ documentation/sql/transport_details.sql
@@ -1,7 +1,5 @@
 CREATE TABLE IF NOT EXISTS `transport_details` (
   `transport_id` BIGINT(20) NOT NULL unique ,
-  `from_warehouseId` varchar(255) NOT NULL,
-  `to_warehouseId` varchar(255) NOT NULL,
   `wareId` varchar(255) NOT NULL,
   `piece` BIGINT(10),
   PRIMARY KEY (from_warehouseId, to_warehouseId, wareId))
