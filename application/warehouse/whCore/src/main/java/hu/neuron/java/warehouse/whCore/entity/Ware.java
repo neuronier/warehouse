16c6beaf2cddd33819ddf16e8c9e768ba7cbd989
Tóth Dávid <skot9212@gmail.com>
2015-08-23 13:35:17 +0200

    ware entity mini update

--- application/warehouse/whCore/src/main/java/hu/neuron/java/warehouse/whCore/entity/Ware.java
+++ application/warehouse/whCore/src/main/java/hu/neuron/java/warehouse/whCore/entity/Ware.java
@@ -4,6 +4,7 @@ import hu.neuron.java.warehouse.whCore.entity.BaseEntity;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -19,7 +20,9 @@ public class Ware extends BaseEntity  {
 	
 
 	private static final long serialVersionUID = 1L;
+	@Column(unique = true, nullable = false)
 	private String wareName;
+	@Column(unique = true, nullable = false)
 	private int itemNumber;
 	private String description;
 	private int visible;
