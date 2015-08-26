cf0089f69d5a3818cc46d1cc67dcd4fcf2bffd94
fodorarpad <hent1989@gmail.com>
2015-08-26 16:57:47 +0200

    Localization valtas bepitese

---
+++ application/warehouse/whWeb/src/main/java/hu/neuron/java/warehouse/whWeb/controller/LocalizationController.java
@@ -0,0 +1,36 @@
+package hu.neuron.java.warehouse.whWeb.controller;
+
+import java.io.Serializable;
+import java.util.Locale;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+
+@SessionScoped
+@ManagedBean(name = "localizationController")
+public class LocalizationController implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private Locale locale;
+
+    @PostConstruct
+    public void init() {
+        locale = FacesContext.getCurrentInstance().getExternalContext().getRequestLocale();
+    }
+
+    public Locale getLocale() {
+        return locale;
+    }
+
+    public String getLanguage() {
+        return locale.getLanguage();
+    }
+
+    public void setLanguage(String language) {
+        locale = new Locale(language);
+        FacesContext.getCurrentInstance().getViewRoot().setLocale(locale);
+    }
+    
+}
